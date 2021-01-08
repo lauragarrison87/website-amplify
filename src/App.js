74a4ac9037dc182097986b00e21664ba06bd5126
lauragarrison <lauragarrison87@gmail.com>
2021-01-08 10:09:13 +0100

    fixed gallery overflow and layout

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ class App extends Component {
                     <Header />
                     <p>Website update in progress. Check back soon!</p>
                 </header>
-
                 <div className="Gallery">
                     <Gallery />
                 </div>

f8553903a261631342ce7995e2c724b2609b5231
lauragarrison <lauragarrison87@gmail.com>
2021-02-06 20:10:56 +0100

    replacement file for old Header.js file

---
+++ src/components/Left.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const Left=()=>
+{
+        return <div className="Header">
+                <h1 className="display-3">
+                Laura Garrison
+                </h1>
+
+                <p>
+                 Visualization PhD Candidate<br/>
+                 University of Bergen, Norway
+                </p>
+
+                <br/>
+                <h2>How do we spatially visualize different biomedical processes? What works, and what doesn't?</h2>
+                <br/>
+                <p>This is a companion site to our ongoing research exploring ways to <strong>more effectively communicate complex concepts in physiology and other biomedical processes</strong>. Explore larger/higher resolution visuals in our asset gallery.</p>
+                <br/>
+                <p>
+                    <em>Interested in participating in our survey? </em>
+                    How do you visualize the spread of cancer to other organs (metastasis)?
+                    How about the ways that we visualize signalling in cells, and what happens when this process goes awry?
+                    What visual strategies make sense for showing blood flow? For an aneurysm?
+                </p>
+                <p>
+                    Email <strong>laura.garrison [at] uib.no</strong> to join in - we appreciate your thoughts and feedback!
+                </p>
+            </div>
+}
+export default Left;

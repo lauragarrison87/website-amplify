6ee7605d6c2ba7eb0a3fd0294d737e16cafc89e8
lauragarrison <lauragarrison87@gmail.com>
2021-04-08 14:09:30 +0200

    moving resume location, updating file locations

--- src/components/Header.js
+++ src/components/Header.js
@@ -24,7 +24,7 @@ class Header extends Component {
          <ul id="nav" className="nav">
             <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
             <li><a className="smoothscroll" href="#about">About</a></li>
-	         <li><a className="smoothscroll" href="#resume">Resume</a></li>
+	         <li><a className="smoothscroll" href="#resume">Experience</a></li>
             <li><a className="smoothscroll" href="#portfolio">Selected Projects</a></li>
          </ul>
 

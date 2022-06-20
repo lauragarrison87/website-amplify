16cec7a628f0880776897d1423665c3aa4da9e3f
lauragarrison <lauragarrison87@gmail.com>
2022-06-20 15:06:40 +0200

    playing with new color palette

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -19,8 +19,8 @@ class Footer extends Component {
            </ul>
 
            <ul className="copyright">
-              <li>&copy; Copyright 2021 Laura Garrison</li>
-              <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
+              <li>&copy; Copyright 2018--2022 Laura Garrison</li>
+              <li>Original design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
            </ul>
 
         </div>
